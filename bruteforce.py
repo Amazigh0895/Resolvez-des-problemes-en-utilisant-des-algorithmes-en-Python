3da50c2c5c0a82c09efc264ee8493afde7ccfc21
Amazigh0895 <arab.fechetah@outlook.fr>
2023-05-28 00:28:48 +0200

    ajout du programme bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -1,45 +1,47 @@
-import pandas as pd
-import numpy as np
+import csv
+import itertools
 
 
-filePath = "dataCsv/dataset1_Python+P7.csv"
-K = 500
+filePath = "dataCsv/dataTest.csv"
+max_budget = 500
+NULL_NUMBER = 0.0
 
 
-# Python3 program to find Closest sum in a list
-def closestSum(lst, K):
-    lst = np.asarray(lst)
-    idx = (np.abs(lst - K)).argmin()
-    return lst[idx]
+with open(filePath, newline="") as file:
+    reader = csv.reader(file, delimiter=',')
+    rows = []
+    for row in reader:
+        if row[0] == "name" or int(row[0]) <= NULL_NUMBER or int(row[1]) <= NULL_NUMBER:
+            pass
+        else:
+            row[1] = float(row[0]) * float(row[1]) / 100
+            rows.append(row)
 
 
-# Python3 program to find Closest index in a list
-def closestIndex(lst, K):
-    lst = np.asarray(lst)
-    idx = (np.abs(lst - K)).argmin()
-    return idx
+def sort_data(data, max_budget):
+    best_combination = []
+    best_profit = []
+    best_profit = 0.0
+    total_cost = 0.0
+    total = 0.0
+    best = 0.0
 
-
-# Read CSV file
-data = pd.read_csv(filePath)
-
-# Sort data by profit
-dataSortedByProfit = data.sort_values(by="profit", ascending=False)
-
-# Sort by sum  of price
-sum = dataSortedByProfit['price'].cumsum()
-
-# Get the closet cost of the actions
-closestSumValue = closestSum(sum, K)
-
-# Get the max index
-closestIndexValue = closestIndex(sum, K)
-
-# Interessting list of best profits
-headInteresstinListProfil = dataSortedByProfit.head(closestIndexValue)
-
-# Get the sum of list profits
-profit = (headInteresstinListProfil['profit'].cumsum()).tail(1)
-
-benifitsMaked = profit.iloc[0] - closestSumValue
-print(benifitsMaked)
+    for r in range(1, len(data) + 1):
+        combinations = itertools.combinations(data, r)
+        for combination in combinations:
+            total_cost = sum(float(item[0]) for item in combination)
+            if total_cost <= max_budget:
+                total_profit = sum(float(item[1]) for item in combination)
+                if total_profit > best_profit:
+                    best_combination.append(combination)
+                    best_profit = total_profit
+    
+    best_combination = best_combination[len(best_combination)-1]
+    for combination in best_combination:
+        print(combination)
+        total += float(combination[0])
+        best += float(combination[1])
+    print(total)
+    print(best)
+ 
+sort_data(rows,max_budget)
