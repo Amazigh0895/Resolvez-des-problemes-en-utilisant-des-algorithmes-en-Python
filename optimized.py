632ce48832c1ea3493d48e4a52b01d5e4335eada
Amazigh0895 <arab.fechetah@outlook.fr>
2023-06-09 20:38:34 +0200

    ajout du programme final  optimized.py

--- optimized.py
+++ optimized.py
@@ -1,5 +1,5 @@
 import csv
-
+import math
 
 filePath = "dataCsv/dataset2_Python+P7.csv"
 max_budget = 500
@@ -10,44 +10,46 @@ NULL_NUMBER = 0.0
 def chargingData():
     with open(filePath, newline="") as file:
         reader = csv.reader(file, delimiter=',')
-        rows = []
+        rows = [] 
         for row in reader:
             if row[0] == "name" or float(row[1]) <= NULL_NUMBER or float(row[2]) <= NULL_NUMBER:
                 pass
             else:
-                row[2] = float(row[1]) * float(row[2]) / 100
+                benifice = float(row[1]) * float(row[2]) / 100
+                rendement = math.floor(max_budget/float(row[1])) * float(row[1]) * (1+ float(row[2]) / 100)  - math.floor(max_budget/float(row[1])) * float(row[1])
+                row.append(benifice)
+                row.append(rendement)
                 rows.append(row)
     return rows
 
 
 rows = chargingData()
-rows = sorted(rows, key=lambda rows: float(rows[2]), reverse=True)
+rows = sorted(rows, key=lambda rows: float(rows[4]), reverse=True)
 
-for row in rows:
-    print(row)
-#print(rows[2][2])
+
 # sort function
 def sort_data(data, max_budget):
-    
     n = len(data)
-    
-    #print(n)
-    total_profit = 0.0
-    total_cost = 0.0
-    i = 0
-
-    while max_budget > 0:
-        if max_budget >= float(data[i][1]):
+    total_profit, total_cost, i, bestListProfit = 0.0, 0.0, 0, []
+    while max_budget > 1:
+        if max_budget >= float(data[i][1]) and i < n-1:
             total_cost += float(data[i][1])
-            print(total_cost)
-            #print(total_profit)
-            total_profit += float(data[i][2])
-        max_budget -= float(data[i][1])
-        i = i+1
-        #print(max_budget)
-        
+            total_profit += float(data[i][3])
+            price = data[i][1]
+            #profit = data[i][3]
+            #bestListProfit.append(name)
+            bestListProfit.append(price)
+            #bestListProfit.append(profit)
+            max_budget -= float(data[i][1])
+            i = i+1
+        else:
+            while max_budget < float(data[i][1]) and i < n-1:
+                i = i + 1
     print("---------------------------")
     print(" La somme des coùt est de ", total_cost, " euros ")
     print(" Le benifice tiré est de ", total_profit, " euros")
+    for profit in bestListProfit:
+        print(profit)
 
-sort_data(rows,max_budget)
+
+sort_data(rows, max_budget)
